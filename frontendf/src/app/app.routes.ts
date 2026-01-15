5be963dbea4190ff44dc017788cd6137ea81a759
Bryan <develop_ids@oceansecurity.net>
2026-01-15 11:55:31 -0500

    Creando componentes y service de persona

--- frontendf/src/app/app.routes.ts
+++ frontendf/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { ClientesComponent } from './pages/clientes/clientes.component';
+import { PersonasComponent } from './pages/personas/personas.component';
 import { InstalacionesComponent } from './pages/instalaciones/instalaciones.component';
-import { PuestosComponent } from './pages/puestos/puestos.component';
-import { HorariosComponent } from './pages/horarios/horarios.component';
 import { AsignacionesComponent } from './pages/asignaciones/asignaciones.component';
-import { ReportesComponent } from './pages/reportes/reportes.component';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './guards/auth.guard';
 
@@ -18,6 +16,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'clientes', pathMatch: 'full' },
       { path: 'clientes', component: ClientesComponent},
+      { path: 'personas', component: PersonasComponent},
       { path: 'instalaciones', component: InstalacionesComponent},
     ]
   },
