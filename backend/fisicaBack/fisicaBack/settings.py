018284f1b048f7f2cf3d70fe9e7883f2e4650568
Bryan <develop_ids@oceansecurity.net>
2026-01-16 17:02:03 -0500

    modificando el tiempo de token

--- backend/fisicaBack/fisicaBack/settings.py
+++ backend/fisicaBack/fisicaBack/settings.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
