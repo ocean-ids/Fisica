dbca41312c7c539a824395bef19075d33b5284a2
Bryan <develop_ids@oceansecurity.net>
2026-01-16 11:22:38 -0500

    estableciendo la cantidad de horas del token de jwt

--- backend/fisicaBack/fisicaBack/settings.py
+++ backend/fisicaBack/fisicaBack/settings.py
@@ -72,8 +72,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=10),  # Token válido por 1 hora
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  # Refresh válido por 7 días
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
