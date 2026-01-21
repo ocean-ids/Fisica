f9863296c33fc3375a9d32c1cf944e4efbfbe42a
Bryan <develop_ids@oceansecurity.net>
2026-01-21 17:21:51 -0500

    modificando nombre del modelo de asignacion estaba mal escrito

--- frontendf/src/app/models/index.ts
+++ frontendf/src/app/models/index.ts
@@ -3,5 +3,5 @@ export * from './instalacion.model';
 export * from './puesto.models';
 export * from './persona.model';
 export * from './horario.models';
-export * from './asignacion.model.ts';
+export * from './asignacion.model';
 
--- frontendf/src/app/pages/asignaciones/asignaciones.component.ts
+++ frontendf/src/app/pages/asignaciones/asignaciones.component.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Asignacion } from '../../models/asignacion.model';
+import { AsignacionService } from '../../services/asignacion.service';
+impo
 
 @Component({
   selector: 'app-asignaciones',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './asignaciones.component.html',
   styleUrl: './asignaciones.component.css'
 })
