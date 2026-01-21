9f76f4f8b8ab10446cef6be11eaf75a9fe88db9e
Bryan <develop_ids@oceansecurity.net>
2026-01-21 16:40:37 -0500

    Agregando modelo de asignacion con sus atributos

---
+++ frontendf/src/app/models/asignacion.model.ts.ts
@@ -0,0 +1,20 @@
+export interface AsignacionModelTs {
+    id?: number;
+    persona: number;
+    instalacion: number;
+    puesto: number;
+    horario:number;
+    mes: number;
+    anio: number;
+    fecha_inicio: string;
+    fecha_fin?: string;
+    rotativo: boolean;
+    orden: number;
+    estado: string;
+    persona_nombre?: string;           
+    cliente_nombre?: string;          
+    instalacion_nombre?: string;       
+    puesto_nombre?: string;            
+    horario_denominativo?: string; 
+}
+
