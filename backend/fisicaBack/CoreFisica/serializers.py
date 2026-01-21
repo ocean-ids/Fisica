eb6c19f5908fe3dea3d699f6948b3b3336750be1
Bryan <develop_ids@oceansecurity.net>
2026-01-21 12:51:42 -0500

    corrigiendo serializers.py

---
+++ backend/fisicaBack/CoreFisica/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Asignacion
+
+class AsignacionSerializer(serializers.ModelSerializer):
+    
+    persona_nombre = serializers.CharField(source='persona.__str__', read_only=True)
+    cliente_nombre = serializers.CharField(source='cliente.nombre_comercial', read_only=True)
+    instalacion_nombre = serializers.CharField(source='instalacion.nombre', read_only=True)
+    puesto_nombre = serializers.CharField(source='puesto.nombre', read_only=True)
+    horario_denominativo = serializers.CharField(source='horario.denominativo', read_only=True)
+    
+    class Meta:
+        model = Asignacion
+        fields = '__all__'
