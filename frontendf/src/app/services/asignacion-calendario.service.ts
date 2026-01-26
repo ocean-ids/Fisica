5bac350d053b1e69e350595f7fc0a5c4d03ee484
Bryan <develop_ids@oceansecurity.net>
2026-01-26 15:06:43 -0500

    consumo de service de AsignacionCalendarioService logica implementada

--- frontendf/src/app/services/asignacion-calendario.service.ts
+++ frontendf/src/app/services/asignacion-calendario.service.ts
@@ -1,14 +1,19 @@
 import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 import { Injectable } from '@angular/core';
+import { AsignacionCalendario } from '../models/asignacion-calendario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsignacionCalendarioService {
+  constructor(private apiService: ApiService){}
 
-  
+  obtenerAsignacionesCalendario(params?: any): Observable<any>{
+    return this.apiService.get<any>('/asignacion-calendario/', params);
+  }
 
-
-  
+  crearAsignacionCalendario(asignacion: AsignacionCalendario): Observable<any>{
+    return this.apiService.post<any>('/asignacion-calendario/crear/', asignacion);
+  }
 }
