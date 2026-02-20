79c89d058a47f7b99183c9df29b9c1f79d107a29
Bryan <develop_ids@oceansecurity.net>
2026-02-20 12:40:39 -0500

    borrando frontend  hecho en  react se me habia olvidado borrarlo XD

--- frontend/fisicaFront/src/Componentes/Asistencia.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Asistencia() {
-  return <h1>Asistencia</h1>;
-}
-
-export default Asistencia;
--- frontend/fisicaFront/src/Componentes/Reportes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Menu from './Menu';
-
-function Reportes() {
-  return <h1>Reportes</h1>;
-  <Menu />
-}
-
-export default Reportes;
