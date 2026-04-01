f9ee8b42b44bf5aba5cfcc8b943ccbeb329e9e1b
Bryan <develop_ids@oceansecurity.net>
2026-04-01 10:53:13 -0500

    creando modal de fromulario del PersonalConsola

---
+++ frontendf/src/app/pages/personal-consola/personal-consola-form/personal-consola-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-consola-form',
+  imports: [],
+  templateUrl: './personal-consola-form.component.html',
+  styleUrl: './personal-consola-form.component.css'
+})
+export class PersonalConsolaFormComponent {
+
+}
