a8dd1b51a6c923d87054ca4547ae0e31bb4e0619
Bryan <develop_ids@oceansecurity.net>
2026-04-16 14:56:33 -0500

    cambiando el label  a Personal Física

--- frontendf/src/app/layout/sidebar/sidebar.component.ts
+++ frontendf/src/app/layout/sidebar/sidebar.component.ts
@@ -15,7 +15,7 @@ export class SidebarComponent {
     { path: '/dashboard/clientes', label: 'Clientes', icon: 'business', permission: 'CoreFisica.view_cliente' },
     { path: '/dashboard/instalaciones', label: 'Instalaciones', icon: 'location_city', permission: 'CoreFisica.view_instalacion' },
     { path: '/dashboard/puestos', label: 'Puestos', icon: 'work', permission: 'CoreFisica.view_puesto' },
-    { path: '/dashboard/personas', label: 'Personal', icon: 'admin_panel_settings', permission: 'CoreFisica.view_persona' },
+    { path: '/dashboard/personas', label: 'Personal Física', icon: 'admin_panel_settings', permission: 'CoreFisica.view_persona' },
     { path: '/dashboard/horarios', label: 'Horarios', icon: 'schedule', permission: 'CoreFisica.view_horario' },
     { path: '/dashboard/asignaciones', label: 'Asignaciones', icon: 'how_to_reg', permission: 'CoreFisica.view_asignacion' },
     { path: '/dashboard/reporte-asistencia', label: 'Reportes Asistencia', icon: 'assignment_ind', permission: 'CoreFisica.view_reporteasistencia' },
