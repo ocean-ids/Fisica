f4682c84fd05bc81f4380eb0f28da8eae04aeab0
Bryan <develop_ids@oceansecurity.net>
2026-05-11 16:25:16 -0500

    carousel en login funcional

--- frontendf/src/app/pages/login/login.component.ts
+++ frontendf/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterModule } from '@angular/router';
@@ -11,19 +11,53 @@ import Swal from 'sweetalert2';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   username: string = '';
   password: string = '';
   isLoading: boolean = false;
+
+  images: string[] = [
+    'assets/images/fondo.png',
+    'assets/images/fondo2.png',
+    'assets/images/fondo3.png'
+  ]
+
+  currentImageIndex = 0;
+  private carouselTimer : any = null;
+
+  get backgroundStyle(): string {
+    const url = this.images[this.currentImageIndex] || this.images[0];
+    return `linear-gradient(rgba(15,31,68,0.20), rgba(15,31,68,0.20)), url('${url}')`;
+  }
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) {}
+  
 
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
       this.router.navigateByUrl('/dashboard', { replaceUrl: true });
+    }
+    this.startCarousel();
+  }
+
+  ngOnDestroy(): void{
+    this.stopCarousel();
+  }
+
+  private startCarousel(): void {
+    if (!this.images.length) return;
+    this.carouselTimer = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 5000);
+  }
+
+  private stopCarousel(): void {
+    if (this.carouselTimer){
+      clearInterval(this.carouselTimer);
+      this.carouselTimer = null;
     }
   }
 
