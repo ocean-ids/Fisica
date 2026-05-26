e38dca4b0653e9b793c0b8bb0aa9861ba243798d
Bryan <develop_ids@oceansecurity.net>
2026-05-26 12:35:04 -0500

    jwt reducito el tiempo de expiracion

--- backend/fisicaBack/fisicaBack/settings.py
+++ backend/fisicaBack/fisicaBack/settings.py
@@ -103,8 +103,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=3),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
