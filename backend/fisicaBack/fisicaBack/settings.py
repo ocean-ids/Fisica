d40aa5e6ccf75d65fa5d4efb8f62e71eec9f485c
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2026-05-31 18:55:28 -0500

    configuracion de settings debug default false

--- backend/fisicaBack/fisicaBack/settings.py
+++ backend/fisicaBack/fisicaBack/settings.py
@@ -44,7 +44,7 @@ def env_list(name, default=''):
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env_bool('DEBUG', default=True)
+DEBUG = env_bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env_list('ALLOWED_HOSTS', default='localhost,127.0.0.1')
 CSRF_TRUSTED_ORIGINS = env_list(
